ba16708842c16d23a68440c76e0230a7647274fe
mishustick123 <71722755+mishustick123@users.noreply.github.com>
2024-05-24 15:05:08 +0300

    shodan.py

--- shodan.py
+++ shodan.py
@@ -12,7 +12,7 @@ options.add_argument("--headless=new")
 
 browser = webdriver.Chrome(options=options)
 
-stealth(browser,
+stealth(browser,                        # fake headers
         languages=["en-US", "en"],
         vendor="Google Inc.",
         platform="Win32",
@@ -29,7 +29,7 @@ final_domain = part1_domain + part2_domain
 
 login_url = 'https://account.shodan.io/login'
 
-txt_dump = open('devices.txt', 'a')
+txt_dump = open('devices.txt', 'a') # dumping results into devices.txt
 
 def login():
     print('Logging into account!')
@@ -37,18 +37,18 @@ def login():
     browser.find_element(by=By.ID, value='username').send_keys('') # username
     browser.find_element(by= By.ID, value='password').send_keys('') #password
     press_enter_xpath = '/html/body/div[2]/main/div/div/div/div[1]/form/div[3]/input'
-    browser.find_element(by=By.XPATH, value=press_enter_xpath).click()  
-    print('Бот успешно зашёл в аккаунт!')
+    browser.find_element(by=By.XPATH, value=press_enter_xpath).click()   # pressing log in button
+    print('Login succesful!')
     time.sleep(2)
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def main(page_counter=1):
     login()     
-    bar = Bar('Scraping', fill='#', suffix='%(percent)d%%')   
+    bar = Bar('Scraping', fill='#', suffix='%(percent)d%%')   # cool progress bar
     for i in range(20):
         browser.get(f'{final_domain}&page={page_counter}')
         soup = BeautifulSoup(browser.page_source, 'html.parser')
-        ips = soup.findAll('a', class_='title text-dark')
+        ips = soup.findAll('a', class_='title text-dark') # finding ip adresses
         for host in ips:
             href = host['href'].split('/')[-1]
             txt_dump.write(f'{href}\n')
